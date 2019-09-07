98fc54b00877252eec5e87fef8d6ffd849b4eb5e
Byron Chavarría <byron.chavarria01@gmail.com>
2019-09-07 11:21:20 -0600

    delete WebApplication solutions

--- BCWebAdmin/Startup.cs
+++ BCWebAdmin/Startup.cs
@@ -56,7 +56,7 @@ namespace BCWebAdmin
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Product}/{action=Index}/{id?}");
             });
         }
     }
