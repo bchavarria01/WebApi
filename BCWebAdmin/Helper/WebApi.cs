6a3dc363530a3ccf1f5a4eead0d60fd3025e1a6f
Byron Chavarría <byron.chavarria01@gmail.com>
2019-09-07 10:58:39 -0600

    last changes

--- BCWebAdmin/Helper/WebApi.cs
+++ BCWebAdmin/Helper/WebApi.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Net.Http;
+
 namespace BCWebAdmin.Helper
 {
     public class WebApi
     {
-        public WebApi()
+        public HttpClient Initial()
         {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:5001/");
+            return client;
         }
     }
 }
